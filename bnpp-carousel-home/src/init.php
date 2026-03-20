460c67dc12dc1eb82f57da28fe6c23df27d379ee
Samuel Coelho <coelhosamuel81@gmail.com.com>
2026-03-20 10:24:55 +0100

    fix error

--- bnpp-carousel-home/src/init.php
+++ bnpp-carousel-home/src/init.php
@@ -132,7 +132,7 @@ function bnpp_carousel_render_block( $attributes ) {
         $link_url = isset( $link['url'] ) ? esc_url( $link['url'] ) : '#';
         $link_class = isset( $link['class'] ) ? sanitize_text_field( $link['class'] ) : 'primary';
 
-        $style = ! empty( $background ) ? ' style="background: linear-gradient(270deg, rgba(12, 39, 40, 0.03) 38.74%, rgba(12, 39, 40, 0.70) 57,43%), url(' . $background . ') lightgray 0px -404px / 100% 200% no-repeat;"' : '';
+        $style = ! empty( $background ) ? ' style="background: linear-gradient(270deg, rgba(12, 39, 40, 0.03) 38.74%, rgba(12, 39, 40, 0.70) 57.43%), url(' . $background . ') lightgray 0px -404px / 100% 200% no-repeat;"' : '';
         $output .= '<div class="bnpp-slide"' . $style . '>';
         $output .= '<div class="bnpp-overlay dark" style="background:transparent">';
         $output .= '<p>' . esc_html( $description ) . '</p>';
